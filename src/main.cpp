ddd43100de42a1530c43ab63cac4a0a93bdb1056
Sharmaharshit0 <harshit.sharma_cs.aiml24@gla.ac.in>
2026-02-27 23:01:20 +0530

    update main.cpp with project documentation and include Servo library

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
-#include <Arduino.h>
-void setup() {
-// write your initialization code here
-}
+/**
+ * @file main.ino
+ * @brief Embedded Force Measurement System using FSR
+ * @author HARSHIT SHARMA
+ * @date 2026-02-27
+ *
+ * @details
+* The MG995 is a high-torque servo motor used for precise angular movement.
+*It can rotate approximately from 0° to 180°.
+*It is commonly used in robotics and automation projects.
+ */
+#include <Servo.h>
 
-
-void loop() {
-// write your code here
-}
+Servo myServo;
